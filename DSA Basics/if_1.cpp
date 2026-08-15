7f60a3ac383a3df30e84809ad93e5894c142fd1e
asimayazmrd0987-glitch <asimayazmrd0987@gmail.com>
2026-08-15 18:16:55 +0500

    dsa basics-

---
+++ DSA Basics/if_1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cout<<"Plaare enter a number :"; cin >> a;
+    if (a>0){
+        cout<<"The number is positive"<<endl;
+    }
+    else{
+        cout<<"The num is negative"<<endl;
+    }
+
+    return 0;
+}
