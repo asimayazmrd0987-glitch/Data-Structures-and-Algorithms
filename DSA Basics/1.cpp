0202bbf91cbbe72384a21eb5193c0edaf335f5d5
asimayazmrd0987-glitch <asimayazmrd0987@gmail.com>
2026-08-15 14:52:56 +0500

    dsa basics

---
+++ DSA Basics/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 12;
+    int b = 21;
+    int sum = a + b;
+    cout<<"The value of the a is :"<<a<<endl; 
+    cout<<"The value of the b is :"<<b<<endl;
+    cout<<"The sum of the a and b is :"<<sum<<endl;
+    return 0;
+}
