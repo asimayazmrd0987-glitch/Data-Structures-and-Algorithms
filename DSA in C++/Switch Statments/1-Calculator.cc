4499d5824604c66557911737222d214af1eafa3a
Asim Ayaz <asimayazmrd0987@gmail.com>
2026-08-28 15:49:13 +0500

    calculator updated

--- DSA in C++/Switch Statments/1-Calculator.cc
+++ DSA in C++/Switch Statments/1-Calculator.cc
@@ -7,21 +7,34 @@ int main()
     char operat;
     cout << "Enter an operator ie(+, -, *, /):";
     cin >> operat;
+    while (operat != '*' &&
+           operat != '-' &&
+           operat != '/' &&
+           operat != '+')
+    {
+        cout << " Invalid Input !" << endl;
+        cin >> operat;
+    }
 
     int num1, num2;
-    cout<<"Enter two intergers to operate them :"; cin>>num1>>num2;
+    cout << "Enter two intergers to operate them :";
+    cin >> num1 >> num2;
 
-    switch( operat ){
+    switch (operat)
+    {
 
-        case '-':cout<<"The consequence is :"<<num1 - num2<<endl;
-            break;
-        case '+':cout<<"The consequence is :"<<num1 + num2<<endl;
-            break;
-        case '*':cout<<"The consequence is :"<<num1 * num2<<endl;
-            break;
-        case '/':cout<<"The consequence is :"<<num1 / num2<<endl;
-            break;
-
+    case '-':
+        cout << "The consequence is :" << num1 - num2 << endl;
+        break;
+    case '+':
+        cout << "The consequence is :" << num1 + num2 << endl;
+        break;
+    case '*':
+        cout << "The consequence is :" << num1 * num2 << endl;
+        break;
+    case '/':
+        cout << "The consequence is :" << num1 / num2 << endl;
+        break;
     }
-    cout<<endl;
-} 
+    cout << endl;
+}
