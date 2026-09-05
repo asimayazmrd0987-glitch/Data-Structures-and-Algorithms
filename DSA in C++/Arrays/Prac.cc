37e81898a10f5f3038c64fad374842d21738fb26
asimayazmrd0987-glitch <asimayazmrd0987@gmail.com>
2026-09-04 22:03:19 -0700

    screenshots

--- DSA in C++/Arrays/Prac.cc
+++ DSA in C++/Arrays/Prac.cc
@@ -3,6 +3,9 @@ using namespace std;
 
 int main()
 {
-    int prac[10];
+    int prac[10] = {0,1,2,3,4,5,6,7,8,9};
+    cout<<"Value at Index 3 is: "<<prac[3]<<endl;
+    cout<<"Value at Index 19 is: "<<prac[19]<<endl;
 
+    return 0;
 }
