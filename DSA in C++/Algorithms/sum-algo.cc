36679ce0f54a3d7c015a0c4d967cec0b8c85ff18
asimayazmrd0987-glitch <asimayazmrd0987@gmail.com>
2026-09-10 20:46:55 +0500

    algorithms

---
+++ DSA in C++/Algorithms/sum-algo.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//  Cubic maximum contiguous subsequence sum algorithm.
+int maxSubSum1(const vector<int> &a)
+{
+    int maxSum = 0;
+
+    for (int i = 0; i < a.size(); ++i)
+        for (int j = i; j < a.size(); ++j)
+        {
+            int thisSum = 0;
+
+            for (int k = i; k <= j; ++k)
+                thisSum += a[k];
+
+            if (thisSum > maxSum)
+                maxSum = thisSum;
+        }
+
+    return maxSum;
+}
+int main() {
+
+    vector<int> a = {95, 130, 70, 5};
+
+    cout <<"Sum is :" << maxSubSum1(a) <<endl;
+    return 0;
+
+}
